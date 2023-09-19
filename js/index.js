1e8486b658e06639ee750197d3bf1c78b02a557b
shjuly29 <shji9607@gmail.com>
2023-09-20 08:42:26 +0900

    ver 4. jsh (230920)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-if (navigator.userAgent.indexOf('iPhone')!= -1) {    
-  addEventListener("load", function() {        
-  setTimeout(hideURLbar, 0);}, false);
-  } else {     
-    hideURLbar();
-  }
-  function hideURLbar(){window.scrollTo(0, 1);}
+history.replaceState({}, null, location.pathname);
