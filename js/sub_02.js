fa9be1d5a8b0c87b6c15033fae8e0f4f0d8f5d9d
shjuly29 <shji9607@gmail.com>
2023-09-25 17:34:36 +0900

    ver 8. jsh (230925)

--- js/sub_02.js
+++ js/sub_02.js
@@ -41,11 +41,27 @@ function prevMove() {
 let startPoint = 0;
 let endPoint = 0;
 
+// PC 클릭 이벤트 (드래그)
+figure.addEventListener("mousedown", (e) => {
+  startPoint = e.pageX; // 마우스 드래그 시작 위치 저장
+});
+
+figure.addEventListener("mouseup", (e) => {
+  endPoint = e.pageX; // 마우스 드래그 끝 위치 저장
+  if (startPoint < endPoint) {
+    // 마우스가 오른쪽으로 드래그 된 경우
+    prevMove();
+  } else if (startPoint > endPoint) {
+    // 마우스가 왼쪽으로 드래그 된 경우
+    nextMove();
+  }
+});
+
 // 모바일 터치 이벤트 (스와이프)
-slide.addEventListener("touchstart", (e) => {
+figure.addEventListener("touchstart", (e) => {
   startPoint = e.touches[0].pageX; // 터치가 시작되는 위치 저장
 });
-slide.addEventListener("touchend", (e) => {
+figure.addEventListener("touchend", (e) => {
   endPoint = e.changedTouches[0].pageX; // 터치가 끝나는 위치 저장
   if (startPoint < endPoint) {
     // 오른쪽으로 스와이프 된 경우
