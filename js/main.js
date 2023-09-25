ceb26224196557004a853e4a3cae758e74c5dfb7
COOLSEONMIN <adc897@naver.com>
2023-09-25 16:04:24 +0900

    ver 1.lsm (230925)

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 
     $(this).siblings(".title").removeClass("active"); 
     $(this).toggleClass("active");
-    $(".content").toggleClass("active");
     $(this).siblings(".content").stop().slideUp();
     $(this).next().stop().slideToggle(); /* title이 나오면 그 다음것 div. 같은 계열 같은 동급의 형제찾기. */
 
