0e62c2fed2fcb75f4b58a70c08125629b2717a63
shjuly29 <shji9607@gmail.com>
2023-09-26 18:04:14 +0900

    ver 9. jsh (230926)

--- js/sub_02.js
+++ js/sub_02.js
@@ -1,73 +1,45 @@
-const figure = document.querySelector("figure");
-let figureWidth = figure.clientWidth;
-const figures = document.querySelectorAll('figure');
-const maxFigure = figures.length;
-
-let currSlide = 1;
-
-window.addEventListener("resize", () => {
-  figureWidth = figure.clientWidth;
-});
+const figures = document.querySelector('.event');
+let st = window.getComputedStyle(figures, null);
+let currDeg = st.getPropertyValue("transform");
 
 function nextMove() {
-  currSlide++;
-  // 마지막 슬라이드 이상으로 넘어가지 않게 하기 위해서
-  if (currSlide <= maxFigure) {
-    // 슬라이드를 이동시키기 위한 offset 계산
-    let offset = figureWidth * (currSlide - 1);
-    // 각 슬라이드 아이템의 left에 offset 적용
-    figures.forEach((i) => {
-      i.setAttribute("style", `left: ${-offset}px`);
-    });
-  } else {
-    currSlide--;
-  }
+  figures.style.transform = `${currDeg}`;
 }
 function prevMove() {
-  currSlide--;
-  // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
-  if (currSlide > 0) {
-    // 슬라이드를 이동시키기 위한 offset 계산
-    let offset = figureWidth * (currSlide - 1);
-    // 각 슬라이드 아이템의 left에 offset 적용
-    figures.forEach((i) => {
-      i.setAttribute("style", `left: ${-offset}px`);
-    });
-  } else {
-    currSlide++;
-  }
+  figures.style.transform = `${currDeg}`;
 }
 
 let startPoint = 0;
 let endPoint = 0;
 
 // PC 클릭 이벤트 (드래그)
-figure.addEventListener("mousedown", (e) => {
-  startPoint = e.pageX; // 마우스 드래그 시작 위치 저장
+figures.addEventListener("mousedown", (e) => {
+  figures.style.animationPlayState = 'paused';
+  startPoint = e.pageX;
 });
 
-figure.addEventListener("mouseup", (e) => {
-  endPoint = e.pageX; // 마우스 드래그 끝 위치 저장
+figures.addEventListener("mouseup", (e) => {
+  endPoint = e.pageX;
   if (startPoint < endPoint) {
-    // 마우스가 오른쪽으로 드래그 된 경우
     prevMove();
-  } else if (startPoint > endPoint) {
-    // 마우스가 왼쪽으로 드래그 된 경우
+  } else {
     nextMove();
   }
+  figures.style.animationPlayState = 'running';
 });
 
 // 모바일 터치 이벤트 (스와이프)
-figure.addEventListener("touchstart", (e) => {
-  startPoint = e.touches[0].pageX; // 터치가 시작되는 위치 저장
+figures.addEventListener("touchstart", (e) => {
+  figures.style.animationPlayState = 'paused';
+  startPoint = e.touches[0].pageX;
 });
-figure.addEventListener("touchend", (e) => {
-  endPoint = e.changedTouches[0].pageX; // 터치가 끝나는 위치 저장
+
+figures.addEventListener("touchend", (e) => {
+  endPoint = e.changedTouches[0].pageX;
   if (startPoint < endPoint) {
-    // 오른쪽으로 스와이프 된 경우
-    prevMove();
-  } else if (startPoint > endPoint) {
-    // 왼쪽으로 스와이프 된 경우
+    prevMove();  
+  } else {
     nextMove();
   }
+  figures.style.animationPlayState = 'running';
 });
